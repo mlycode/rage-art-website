405a08a28e0fc556f0b23e38548942ba88affe3f
mlycode <m.ly@live.co.uk>
2019-01-03 18:01:13 +0000

    add artworkCard component

---
+++ src/components/ArtworkCard/ArtworkCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./ArtworkCard.scss";
+
+const ArtworkCard = (props) => {
+    return (
+        <div className="artwork">
+            <img className="artwork-card__image" src={props.img} alt={props.img} />
+            <h2 className="artwork-card--title">{props.title}</h2>
+            <div className="artwork-card__description">
+                <h3 className="artwork-card__description--title">{props.title}, {props.year}</h3>
+                <h4 className="artwork-card__description--subtitle">{props.subtitle}</h4>
+                <p className="artwork-card__description--content">{props.children}</p>
+            </div>
+        </div>
+    );
+}
+
+export default ArtworkCard;
