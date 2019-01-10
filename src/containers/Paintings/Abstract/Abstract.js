d1f05bd7f0f8adcdfeb7555e7b541ab65ef599ae
mlycode <m.ly@live.co.uk>
2019-01-10 14:28:55 +0000

    update abstract page content

--- src/containers/Paintings/Abstract/Abstract.js
+++ src/containers/Paintings/Abstract/Abstract.js
@@ -27,22 +27,22 @@ class AbstractPage extends Component {
                     <p>Use your imagination, let it go. But they're very easily killed. Clouds are delicate. You have to make those little noises or it won't work. Let's put some highlights on these little trees. The sun wouldn't forget them. Let's give him a friend too. Everybody needs a friend.</p>
                     <p>You can get away with a lot. Now let's put some happy little clouds in here. Let's make a happy little mountain now. Everyone needs a friend. Friends are the most valuable things in the world. Let's get wild today. Little trees and bushes grow however makes them happy.</p>
                 </ArtDesc>
-                <ArtworkCard img={img1} title="Ya Butt" year="2016" subtitle="Oil on Canvas, 70cm x 50cm">
+                <ArtworkCard img={img1} title="Rainbow" year="2016" subtitle="Oil on Canvas, 70cm x 50cm">
                     We'll put all the little clouds in and let them dance around and have fun. This piece of canvas is your world. Take your time. Speed will come later. These things happen automatically. All you have to do is just let them happen. It's amazing what you can do with a little love in your heart. God gave you this gift of imagination. Use it.
                 </ArtworkCard>
-                <ArtworkCard img={img2} title="Ya Butt" year="2016" subtitle="Oil on Canvas, 70cm x 50cm">
+                <ArtworkCard img={img2} title="Chaos Flower" year="2016" subtitle="Oil on Canvas, 70cm x 50cm">
                     We'll put all the little clouds in and let them dance around and have fun. This piece of canvas is your world. Take your time. Speed will come later. These things happen automatically. All you have to do is just let them happen. It's amazing what you can do with a little love in your heart. God gave you this gift of imagination. Use it.
                 </ArtworkCard>
-                <ArtworkCard img={img3} title="Ya Butt" year="2016" subtitle="Oil on Canvas, 70cm x 50cm">
+                <ArtworkCard img={img3} title="Red Giant" year="2016" subtitle="Oil on Canvas, 70cm x 50cm">
                     We'll put all the little clouds in and let them dance around and have fun. This piece of canvas is your world. Take your time. Speed will come later. These things happen automatically. All you have to do is just let them happen. It's amazing what you can do with a little love in your heart. God gave you this gift of imagination. Use it.
                 </ArtworkCard>
-                <ArtworkCard img={img4} title="Ya Butt" year="2016" subtitle="Oil on Canvas, 70cm x 50cm">
+                <ArtworkCard img={img4} title="Fire Lake" year="2016" subtitle="Oil on Canvas, 70cm x 50cm">
                     We'll put all the little clouds in and let them dance around and have fun. This piece of canvas is your world. Take your time. Speed will come later. These things happen automatically. All you have to do is just let them happen. It's amazing what you can do with a little love in your heart. God gave you this gift of imagination. Use it.
                 </ArtworkCard>
-                <ArtworkCard img={img5} title="Ya Butt" year="2016" subtitle="Oil on Canvas, 70cm x 50cm">
+                <ArtworkCard img={img5} title="Chilling" year="2016" subtitle="Oil on Canvas, 70cm x 50cm">
                     We'll put all the little clouds in and let them dance around and have fun. This piece of canvas is your world. Take your time. Speed will come later. These things happen automatically. All you have to do is just let them happen. It's amazing what you can do with a little love in your heart. God gave you this gift of imagination. Use it.
                 </ArtworkCard>
-                <ArtworkCard img={img6} title="Ya Butt" year="2016" subtitle="Oil on Canvas, 70cm x 50cm">
+                <ArtworkCard img={img6} title="Ocean Blue" year="2016" subtitle="Oil on Canvas, 70cm x 50cm">
                     We'll put all the little clouds in and let them dance around and have fun. This piece of canvas is your world. Take your time. Speed will come later. These things happen automatically. All you have to do is just let them happen. It's amazing what you can do with a little love in your heart. God gave you this gift of imagination. Use it.
                 </ArtworkCard>
             </Fragment>
