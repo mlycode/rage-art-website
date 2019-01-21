02e74b6455943298787a978a1fff0b1ac7cae774
mlycode <m.ly@live.co.uk>
2019-01-21 13:02:01 +0000

    edit back link button component to use svg as icon

--- src/components/BackLinkButton/BackLinkButton.js
+++ src/components/BackLinkButton/BackLinkButton.js
@@ -2,11 +2,12 @@ import React from "react";
 
 import "./BackLinkButton.scss";
 import { Link } from "react-router-dom";
+import backArrow from "../../assets/icons/left-arrow.svg";
 
 const BackLinkButton = (props) => {
     return (
         <div className="back-button">
-            <Link to={props.link}>&#8678;</Link>
+            <Link to={props.link}><img src={backArrow} alt="Go Back"/></Link>
         </div>
     );
 }
