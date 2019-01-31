674d85a6482d41ad01e67da093a274b51ab12101
mlycode <m.ly@live.co.uk>
2019-01-31 16:22:14 +0000

    update rage paintings page content

--- src/components/Navbar/Footer/Footer.js
+++ src/components/Navbar/Footer/Footer.js
@@ -6,7 +6,7 @@ import "./Footer.scss";
 const Footer = () => {
     return (
         <footer className="nav__footer">
-            <Link exact to="/commisions">Contact me</Link>
+            <Link exact to="/contact">Contact me</Link>
             <a href="https://www.instagram.com/therageart/?hl=en" target="none">Instagram</a>
             <p>Copyright &copy; 2019</p>
             <p>Made with &hearts; mlycode</p>
